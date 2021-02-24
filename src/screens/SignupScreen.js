e5f5cca3ca53546873cc52a4dfd19f4d0ef19328
nguyenhung <hunghung0309@gmail.com>
2021-02-24 09:24:50 +0700

    206

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button} from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return(
-        <>
-            <Text>Signup Screen</Text>
-            <Button 
+        <View style={styles.container}>  
+            <Spacer>
+                <Text h3 style={{textAlign: 'center'}}>Sign Up for NNH</Text> 
+            </Spacer>
+            <Spacer>
+                <Input 
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+                />
+            </Spacer>
+            <Spacer>
+                <Input 
+                secureTextEntry
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false} />
+            </Spacer>
+            <Spacer>
+                <Button title = "Sign Up" />
+            </Spacer>
+            <Button
                 title="Go to Signin"
                 onPress={() => navigation.navigate('Signin')}
             />
@@ -13,10 +41,25 @@ const SignupScreen = ({ navigation }) => {
                 title="Go to main flow"
                 onPress={() => navigation.navigate('mainFlow')}
             />
-        </>
-    )
-}
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 5, 
+        borderColor: 'red',
+        flex: 1,
+        justifyContent: 'center', 
+        marginBottom: 200
+    }
+});
 
 export default SignupScreen;
